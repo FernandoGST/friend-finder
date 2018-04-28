5681aab071ed48276149b106385a99db820538ff
Fgallegos <fgallegosstanford@outloo.com>
2018-04-28 08:10:13 -0500

    test

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Wade Wilson',
+		photo: 'https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/02/08/Pictures/_33b2ca74-0cc1-11e8-ba67-a8387f729390.jpeg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Peter Quill',
+		photo: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/0/09/StarLord-Guardians2-Profile.jpg/revision/latest/scale-to-width-down/314?cb=20170404034947',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Logan',
+		photo: 'https://fsmedia.imgix.net/b3/2d/b0/86/63e0/475b/a858/467481d88bb5/hugh-jackman-as-wolverine.jpeg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Steve Rogers',
+		photo: 'https://pbs.twimg.com/profile_images/937142070992605184/GO70zooO_400x400.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Tony Stark',
+		photo: 'https://www.telegraph.co.uk/content/dam/films/2016/04/28/tonystark2_trans_NvBQzQNjv4BqeuRHplZSizlnIpEPA_wgci-oMky92GODLj7YayikDrk.jpg?imwidth=450',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Pepper Potts',
+		photo: 'https://static.comicvine.com/uploads/original/12/128267/2494317-pepper_potts_super.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'T.Challa',
+		photo: 'https://ohmy.disney.com/wp-content/uploads/2017/07/TChalla-Black-Panther-EW-1200x800.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Natalia Alianovna Romanova',
+		photo: 'https://www.screengeek.net/wp-content/uploads/2018/04/black-widow-movie.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Nick Fury',
+		photo: 'http://digitalspyuk.cdnds.net/17/33/980x490/landscape-1502794376-samuel-l-jackson-nick-fury-side-eye.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Wanda Maximoff',
+		photo: 'https://www.buymoviejackets.com/wp-content/uploads/2016/04/Skinny-Sleeves-Scarlet-Witch-Red-Costume-Coat-450x600.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
